6192bc679f88dbbe7e0dfbcd7b4797613d50ad14
fawadashraf1262 <fawadashraf1262@gmail.com>
2023-10-03 22:27:33 +0500

    e

--- script.js
+++ script.js
@@ -21,12 +21,7 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
-
-  // console.log('A key is pressed');
-  // modal.classList.add('hidden');
-  // overlay.classList.add('hidden');
 });
